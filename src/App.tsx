692561460780aec363e9b305f6d859e0a160ee82
vvu060 <vvu060@gmail.com>
2024-06-24 15:44:46 +0530

    add dragable div

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import { motion, useMotionValue } from 'framer-motion';
 
 function App() {
   return (
@@ -9,13 +10,21 @@ function App() {
 }
 
 const FoldableMap = () => {
+  const xDrag = useMotionValue(0);
+
   return (
     <div className='mx-auto grid aspect-video max-h-[80vh] p-8'>
-      <div className='aspect-video grid grid-cols-3 h-full w-full'>
+      <div className='aspect-video grid grid-cols-3 h-full w-full [grid-area:1/1]'>
+        <div className='map-image translate-x-full' />
         <div className='map-image' />
-        <div className='map-image' />
-        <div className='map-image' />
+        <div className='map-image -translate-x-full' />
       </div>
+      <motion.div
+        drag='x'
+        style={{ x: xDrag }}
+        dragConstraints={{ left: 0, right: 300 }}
+        className='[grid-area:1/1] border border-black relative z-10'
+      />
     </div>
   );
 };
