8647300e418a3b3fce6620365970188bc49d8e75
vvu060 <vvu060@gmail.com>
2024-06-24 15:49:08 +0530

    prevent drag trigger area from moving while dragging

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './styles.css';
-import { motion, useMotionValue } from 'framer-motion';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 
 function App() {
   return (
@@ -11,19 +11,27 @@ function App() {
 
 const FoldableMap = () => {
   const xDrag = useMotionValue(0);
+  const xLeftSection = useTransform(xDrag, [0, 300], ['100%', '0%']);
+  const xRightSection = useTransform(xDrag, [0, 300], ['100%', '0%']);
 
   return (
     <div className='mx-auto grid aspect-video max-h-[80vh] p-8'>
       <div className='aspect-video grid grid-cols-3 h-full w-full [grid-area:1/1]'>
-        <div className='map-image translate-x-full' />
-        <div className='map-image' />
-        <div className='map-image -translate-x-full' />
+        <motion.div
+          style={{ x: xLeftSection }}
+          className='map-image translate-x-full'
+        />
+        <motion.div className='map-image' />
+        <motion.div
+          style={{ x: xRightSection }}
+          className='map-image -translate-x-full'
+        />
       </div>
       <motion.div
         drag='x'
-        style={{ x: xDrag }}
+        _dragX={xDrag}
         dragConstraints={{ left: 0, right: 300 }}
-        className='[grid-area:1/1] border border-black relative z-10'
+        className='[grid-area:1/1]  relative z-10'
       />
     </div>
   );
