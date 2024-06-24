b0f9ad4156b1a36bf9b8325349a1fabd87eb2b89
vvu060 <vvu060@gmail.com>
2024-06-24 15:53:59 +0530

    add shadow to center position

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import './styles.css';
-import { motion, useMotionValue, useTransform } from 'framer-motion';
+import {
+  MotionStyle,
+  motion,
+  useMotionValue,
+  useTransform,
+} from 'framer-motion';
 
 function App() {
   return (
@@ -12,26 +17,30 @@ function App() {
 const FoldableMap = () => {
   const xDrag = useMotionValue(0);
   const xLeftSection = useTransform(xDrag, [0, 300], ['100%', '0%']);
-  const xRightSection = useTransform(xDrag, [0, 300], ['100%', '0%']);
+  const xRightSection = useTransform(xDrag, [0, 300], ['-100%', '0%']);
+  const centerScale = useTransform(xDrag, [150, 300], [0, 1]);
+  const centerBrightness = useTransform(xDrag, [150, 300], [0.2, 1]);
 
   return (
     <div className='mx-auto grid aspect-video max-h-[80vh] p-8'>
       <div className='aspect-video grid grid-cols-3 h-full w-full [grid-area:1/1]'>
+        <motion.div style={{ x: xLeftSection }} className='map-image' />
         <motion.div
-          style={{ x: xLeftSection }}
-          className='map-image translate-x-full'
+          style={
+            {
+              scaleX: centerScale,
+              '--brightness': centerBrightness,
+            } as MotionStyle
+          }
+          className='map-image brightness-[--brightness]'
         />
-        <motion.div className='map-image' />
-        <motion.div
-          style={{ x: xRightSection }}
-          className='map-image -translate-x-full'
-        />
+        <motion.div style={{ x: xRightSection }} className='map-image' />
       </div>
       <motion.div
         drag='x'
         _dragX={xDrag}
         dragConstraints={{ left: 0, right: 300 }}
-        className='[grid-area:1/1]  relative z-10'
+        className='[grid-area:1/1] relative z-10'
       />
     </div>
   );
