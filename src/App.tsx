93304e376b128e8000310e2ba2aec0937c198b42
vvu060 <vvu060@gmail.com>
2024-06-25 09:55:30 +0530

    add bounce when map opens

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import './styles.css';
 import {
   MotionStyle,
   motion,
   useMotionValue,
+  useMotionValueEvent,
   useTransform,
 } from 'framer-motion';
 
@@ -20,40 +22,60 @@ const FoldableMap = () => {
   const xRightSection = useTransform(xDrag, [0, 300], ['-100%', '0%']);
   const centerScale = useTransform(xDrag, [150, 300], [0, 1]);
   const centerBrightness = useTransform(xDrag, [150, 300], [0.2, 1]);
+  const [isFolded, setIsFolded] = useState(false);
+
+  useMotionValueEvent(xDrag, 'change', (currentX) => {
+    if (currentX > 260) {
+      setIsFolded(false);
+    } else {
+      setIsFolded(true);
+    }
+  });
 
   return (
-    <div className='mx-auto grid aspect-video max-h-[80vh] p-8'>
-      <div className='aspect-video grid grid-cols-3 h-full w-full [grid-area:1/1]'>
-        <motion.div
-          style={{ x: xLeftSection, skewY: '-1deg' }}
-          className='map-image origin-bottom-right border-r border-[rgba(255,255,255,.1)] shadow-2xl'
-        />
-        <motion.div
-          style={
-            {
-              scaleX: centerScale,
-              '--brightness': centerBrightness,
-            } as MotionStyle
-          }
-          className='map-image brightness-[--brightness]'
-        />
-        <motion.div
-          style={{ x: xRightSection, skewY: '1deg' }}
-          className='map-image origin-bottom-left border-l border-[rgba(255,255,255,.1)] shadow-2xl'
-        />
-      </div>
+    <div className='overflow-clip'>
       <motion.div
-        drag='x'
-        _dragX={xDrag}
-        dragConstraints={{ left: 0, right: 300 }}
-        dragTransition={{
-          modifyTarget: (target) => {
-            return target > 150 ? 300 : 0;
-          },
-          timeConstant: 45,
+        animate={isFolded ? 'folded' : 'open'}
+        variants={{
+          open: { scale: 1 },
+          folded: { scale: 0.9 },
         }}
-        className='[grid-area:1/1] relative z-10'
-      />
+        initial='folded'
+      >
+        <div className='mx-auto grid aspect-video max-h-[80vh] p-8'>
+          <div className='aspect-video grid grid-cols-3 h-full w-full [grid-area:1/1]'>
+            <motion.div
+              style={{ x: xLeftSection, skewY: '-1deg' }}
+              className='map-image origin-bottom-right border-r border-[rgba(255,255,255,.1)] shadow-2xl'
+            />
+            <motion.div
+              style={
+                {
+                  scaleX: centerScale,
+                  '--brightness': centerBrightness,
+                } as MotionStyle
+              }
+              className='map-image brightness-[--brightness]'
+            />
+            <motion.div
+              style={{ x: xRightSection, skewY: '1deg' }}
+              className='map-image origin-bottom-left border-l border-[rgba(255,255,255,.1)] shadow-2xl'
+            />
+          </div>
+          <motion.div
+            drag='x'
+            _dragX={xDrag}
+            dragConstraints={{ left: 0, right: 300 }}
+            dragTransition={{
+              modifyTarget: (target) => {
+                return target > 150 ? 300 : 0;
+              },
+              timeConstant: 45,
+            }}
+            className='[grid-area:1/1] relative z-10'
+          />
+        </div>
+      </motion.div>
     </div>
   );
 };
